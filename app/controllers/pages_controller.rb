9b15d6ed39a30221356bd3d4cb76da37a7b07f3b
Tyler Rickard <t.rickard7@hotmail.com>
2018-11-19 19:43:13 -0600

    FEATURE: added more messages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,15 @@ class PagesController < ApplicationController
 
     sender_number = params["From"]
 
+    incoming_message = params["Body"]
+
     message= "Hello from Tyler's computer. This app would be fun to mess with people."
+
+    if incoming_message == "secret"
+      message = "Oblivion is better than Skyrim."
+    else
+      message = "This is a boring text message."
+    end
 
     @client = Twilio::REST::Client.new(twilio_sid, twilio_token)
     @client.messages.create(
