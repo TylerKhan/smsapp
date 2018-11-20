b7de8cf3cec31be1c535c654e48461c2f9d55724
Tyler Rickard <t.rickard7@hotmail.com>
2018-11-19 19:01:58 -0600

    FEATURE: setting up twilio client

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,17 @@ class PagesController < ApplicationController
   end
 
   def incoming_sms
+    twilio_sid = ENV['TWILIO_SID']
+    twilio_token = ENV['TWILIO_TOKEN']
+    twilio_phone_number = ENV['TWILIO_PHONE_NUMBER']
+    
+    @client = Twilio::REST::Client.new(twilio_sid, twilio_token)
+    @client.messages.create(
+      :from => twilio_phone_number,
+      :to => sender_number,
+      :body => message
+      )
+
+    render :nothung => true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/' => 'pages#index'
   get 'index' => 'pages#index'
   post 'incoming_sms' => 'pages#incoming_sms'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
